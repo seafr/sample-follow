646d8e95dc7018becaa689e57f82f31b67713454
Sea <seafr@users.noreply.github.com>
2017-10-13 10:56:49 +0400

    Update follow/unfollow to work on logged-in user, not Admin.

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,13 @@
 class RelationshipsController < ApplicationController
   def create
-    user = User.find(1)
+    user = User.find(session[:user_id])
 	other_user = User.find(params[:followed_id])
 	user.follow(other_user)
 	redirect_to other_user
   end
 
   def destroy
-    user = User.find(1)
+    user = User.find(session[:user_id])
 	other_user = Relationship.find(params[:id]).followed
 	user.unfollow(other_user)
 	redirect_to other_user
