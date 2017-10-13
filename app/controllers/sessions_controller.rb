0bec96371fadfdba1ba82c387ccabd6e42ed8945
Sea <seafr@users.noreply.github.com>
2017-10-13 10:46:57 +0400

    Create action in Sessions controller for logout. Add links for login/logout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,4 +11,9 @@ class SessionsController < ApplicationController
 	  render 'new'
 	end
   end
+
+  def destroy
+    session.delete(:user_id) if session[:user_id]
+	redirect_to login_url
+  end
 end
