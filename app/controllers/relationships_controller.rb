ffff4ff6ec4e7ee55dea0aaea540f0c0dedf074c
Sea <seafr@users.noreply.github.com>
2017-10-13 10:17:05 +0400

    Add unfollow button to user profile. Create action in Relationships controller for unfollowing a user.

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,15 @@
 class RelationshipsController < ApplicationController
   def create
-    user = User.find(2)
+    user = User.find(1)
 	other_user = User.find(params[:followed_id])
 	user.follow(other_user)
+	redirect_to other_user
+  end
+
+  def destroy
+    user = User.find(1)
+	other_user = Relationship.find(params[:id]).followed
+	user.unfollow(other_user)
 	redirect_to other_user
   end
 end
